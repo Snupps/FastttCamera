009aeef9ede2df5ca43f0dd87a450b4ebb9b0095
Peyman Khanjan <p.khanjan@gmail.com>
2016-10-22 19:23:58 +0100

    Carthage support.

---
+++ FastttCameraFramework/FastttCameraFramework.h
@@ -0,0 +1,13 @@
+//
+//  FastttCameraFramework.h
+//  FastttCameraFramework
+//
+//  Created by Peyman Khanjan on 22/10/2016.
+//  Copyright © 2016 Snupps. All rights reserved.
+//
+
+#import <FastttCameraFramework/FastttCamera.h>
+#import <FastttCameraFramework/FastttCameraInterface.h>
+#import <FastttCameraFramework/FastttCameraTypes.h>
+#import <FastttCameraFramework/FastttCapturedImage.h>
+#import <FastttCameraFramework/UIViewController+FastttCamera>
